14cfe5b4598383e5ae6afb30f42dddc14aa4b07f
Joshua Call <joshuacall@protonmail.com>
2018-09-11 12:19:17 -0600

    Made some changes to src/battery.hpp to make it sleep for 1 second, which will now be obsolete with the new documentation. Thought I would commit it anyway so that we would have a record of it.

--- src/battery.hpp
+++ src/battery.hpp
@@ -3,14 +3,22 @@
  * Defines the BatterySentinel class.
  */
 
+#include <unistd.h>
 #ifndef BATTERY_INCLUDED
 #define BATTERY_INCLUDED
 
-#include "peripheral/uc_board.hpp"
 
 class BatterySentinel
 {
-
+  public:
+    void sentinel()
+    {
+      while(ANY_MOTOR_RUNNING)
+      {
+        sleep(1); //The documentation says to sleep for a few seconds, but 
+                  //wouldn't that be an awfully long time?
+      }
+    }
 };
 
 #endif
