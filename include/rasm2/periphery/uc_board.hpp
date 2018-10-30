5cb587c91c4696bea7463fbc89adb3065e2d8886
Joe Pollard <41n112w@gmail.com>
2018-10-30 12:56:57 -0600

    Updated all comments and removed singleton restriction.

--- include/rasm2/periphery/uc_board.hpp
+++ include/rasm2/periphery/uc_board.hpp
@@ -1,5 +1,5 @@
 /**
- * Defines the UCBoard (singleton) class.
+ * Defines the UCBoard class.
  */
 
 #ifndef RASM2_PERIPHERY_UC_BOARD_HPP
@@ -14,46 +14,82 @@
 #include "configuration.hpp"
 
 /**
- * AVR uController; UART slave; motor driving / encoder reading / power
- *     managment board signal reading.
+ * This class acts as the service layer on top of the UART communication port
+ * connected to the microcontroller on the signal processing board (termed
+ * UCBoard here as short for microcontroller board). The 'services' provided
+ * by an instance of this class are the following:
+ *   - is connected
+ *   - is power on
+ *   - get battery voltage
+ *   - get encoder outputs
+ *   - set motor pwm levels
+ * The microcontroller is communicated with as a stateless synchronous-response
+ * peripheral.
  */
 class UCBoard
 {
 private:
-  serial_t *uart_port;
-
-  UCBoard()
-  {
-    Poco::AutoPtr<MapConfiguration> configs = ConfigurationManager::get_instance().
-        get_config_group(ConfigurationManager::Group::PERIPHERY);
-    int uartbus = configs->getInt("uc_board.uartbus");
-
-    UartConf uartconf;  // leave defaults
-    uartconf.baudrate = BaudRate::_115200;
-    bool success = PeripheryAccess::get_instance().
-        init_uart_device(uartbus, &uart_port, uartconf);
-    if (!success)
-    {
-      throw Poco::RuntimeException("In UCBoard::UCBoard()\n"
-      "Initialization failed for UART device number " + std::to_string(uartbus) + ".");
-    }
-  }
+  serial_t *uartport;
 
 public:
   UCBoard(const UCBoard &) = delete;
   void operator=(const UCBoard &) = delete;
 
   /**
-   * Returns a reference to this singleton's instance.
+   * Constructs a new UCBoard instance that uses the given configured uart port to
+   * communicate to the microcontroller.
    */
-  static UCBoard& get_instance()
+  UCBoard(serial_t *port)
+  : uartport(port)
   {
-    static UCBoard board;
-    return board;
   }
 
   /**
+   * Returns a pointer to the serial_t instance that represents the designated
+   * uart port as specified in the configuration subsystem to use for
+   * communicating to the microcontroller.
+   */
+  static serial_t * get_default_port()
+  {
+    Poco::AutoPtr<MapConfiguration> configs = ConfigurationManager::get_instance().
+        get_config_group(ConfigurationManager::Group::PERIPHERY);
+    int uartbus = configs->getInt("uc_board.uartbus");
+
+    serial_t *uartport;
+    UartConf uartconf;  // leave defaults
+    uartconf.baudrate = BaudRate::_115200;
+    bool success = PeripheryAccess::get_instance().
+        init_uart_device(uartbus, &port, uartconf);
+    if (!success)
+    {
+      throw Poco::RuntimeException("In UCBoard::UCBoard()\n"
+      "Initialization failed for UART device number " + std::to_string(uartbus) + ".");
+    }
+
+    return uartport;
+  }
+
+  /**
+   * Returns true if the uC board is connected and communicating properly.
    *
+   * Specification for signal processing microcontroller (uC):
+   *   - communication over UART port at baud rate of 115200
+   *   - data sent to uC: {BEL}
+   *   - data received from uC: {ACK}
+   */
+  bool connected()
+  {
+    return false;
+  }
+
+  /**
+   * Returns true if the power management board is still switched on; false if
+   * not.
+   *
+   * Specification for signal processing microcontroller (uC):
+   *   - communication over UART port at baud rate of 115200
+   *   - data sent to uC: {'p'}
+   *   - data received from uC: {'[0-1]'}
    */
   bool is_pmb_on()
   {
@@ -61,7 +97,13 @@ public:
   }
 
   /**
+   * Returns the current battery voltage with a resolution of 0.029.
+   * (resolution = division_factor * V_adc * 2^-bits = 6*5*2^-10 = 0.029)
    *
+   * Specification for signal processing microcontroller (uC):
+   *   - communication over UART port at baud rate of 115200
+   *   - data sent to uC: {'b'}
+   *   - data received from uC: {'[0-1]', '[0-9]', '[0-9]', '[0-9]'}
    */
   double get_battery_voltage()
   {
@@ -69,30 +111,54 @@ public:
   }
 
   /**
+   * Returns the values of all six encoders. These values are plain outputs
+   * from the ADCs reading the encoder outputs.
    *
+   * Specification for signal processing microcontroller (uC):
+   *   - communication over UART port at baud rate of 115200
+   *   - data sent to uC: {'e'}
+   *   - data received from uC: 6 x {'[0-1]', '[0-9]', '[0-9]', '[0-9]'}
    */
-  std::array<unsigned char, 6> get_md_errors()
-  {
-    std::array<unsigned char, 6> empty;
-    return empty;
-  }
-
-  /**
-   *
-   */
-  std::array<int, 6> get_encoder_positions()
+  std::array<int, 6> get_encoder_outputs()
   {
     std::array<int, 6> empty;
     return empty;
   }
 
   /**
+   * Sets the PWM levels for all six motors. Each PWM signal is a floating
+   * point value in the range [-100, 100]. The magnitude of this value
+   * represents the duty cycle while the sign is the motor direction. If a PWM
+   * level is outside of this range then it will be changed to the range
+   * boundary that is closest.
    *
+   * Specification for signal processing microcontroller (uC):
+   *   - communication over UART port at baud rate of 115200
+   *   - data sent to uC: {'m'}, 6 x {'[0-1]', '[0-1]', '[0-9]', '[0-9]', '[0-9]'}
+   *   - data received from uC: {ACK}
    */
-  void set_motor_pwms(std::array<float, 6> pwmlevels)
+  bool set_motor_pwms(std::array<float, 6> pwmlevels)
   {
     return;
   }
+
+  /**
+   *
+   */
+  /*std::array<unsigned char, 6> get_md_currents()
+  {
+    std::array<unsigned char, 6> empty;
+    return empty;
+  }*/
+
+  /**
+   *
+   */
+  /*std::array<unsigned char, 6> get_md_errors()
+  {
+    std::array<unsigned char, 6> empty;
+    return empty;
+  }*/
 };
 
 #endif
