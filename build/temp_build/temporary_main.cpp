c2f861ea7414abb9d38a6ec9bb855f139536e972
Joshua Call <joshuacall@protonmail.com>
2018-12-03 10:38:59 -0700

    Created the first version of the pose.hpp file, with two separte classes.

--- build/temp_build/temporary_main.cpp
+++ build/temp_build/temporary_main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include "../../../rasm-v2/include/rasm2/control/trajectory_generation.hpp"
+#include "../../../rasm-v2/include/rasm2/vision/pose.hpp"
 int main(int argc, char** argv)
 {
-
+FacePoseEstimator face(1);
+face.get_pose();
+//MarkerPoseEstimator marker(1);
 }
