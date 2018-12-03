cd00a206b744b61d9928f251b0626f657f9ea21a
Joshua Call <joshuacall@protonmail.com>
2018-12-03 09:23:41 -0700

    started pose.hpp

---
+++ include/rasm2/vision/pose.hpp
@@ -0,0 +1,20 @@
+/*
+ * Defines the FacePoseEstimator and MarkerPoseEstimator classes.
+ */
+
+#ifndef RASM2_VISION_POSE_ESTIMATION_HPP
+#define RASM2_VISION_POSE_ESTIMATION_HPP
+
+
+class FacePoseEstimator
+{
+
+
+};
+
+class MarkerPoseEstimator 
+{
+
+
+};
+#endif
